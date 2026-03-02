d3f527d324f858868523958132f8451bb28f20c1
GlebIstep <glebasik52525@gmil.com>
2026-03-02 17:10:34 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,19 @@
+﻿#include "logic.h"
+// Task 01 [The count of non - zero values]
+// Количество ненулевых значений
+// 
+// Дан вектор целочисленных значений. Необходимо спроектировать 
+// и реализовать функцию, которая подсчитывает количество ненулевых
+// значений элементов вектора.
+
+int count_nonzero_elements(int array[DEFAULT_SIZE], int size) {
+	if (size <= 0) return -1;
+
+	int count = 0;
+	for (int index = 0; index < size; index++) {
+		if (array[index] != 0) {
+			count++;
+		}
+	}
+	return count;
+}
